b8f300379bf225fea8b4492f3a19122c1055d11f
Eldar <eldarbotyr@mail.ru>
2023-05-20 12:56:25 -0400

    added functionality to workshop service list

--- Workshop/assets/scripts/main.js
+++ Workshop/assets/scripts/main.js
@@ -2,8 +2,23 @@ import '../styles/app.css'
 const preview = document.querySelector('#preview');
 const header = document.querySelector('header');
 const ul = document.querySelector('ul');
-preview.style.top = header.offsetHeight-1;
-preview.style.height = `calc(90vh - ${header.offsetHeight}px)`;
+const openButtons = document.getElementsByClassName('open-buttons')
+const actionsPopUp = document.querySelector('#actions-list');
+const headerOffset = header.offsetHeight;
+preview.style.marginTop = headerOffset-10;
+preview.style.height = `calc(88vh - ${headerOffset}px)`;
+
+actionsPopUp.style.marginTop = headerOffset;
+actionsPopUp.style.height = `calc(100vh - ${headerOffset}px) `
+
+actionsPopUp.addEventListener('click', ()=>{alert('asd')})
+
 ul.addEventListener('click', event => {
     event.target.closest('li').lastChild.classList.toggle('service-active');
 })
+
+for (const button of openButtons){
+button.addEventListener('click', () => {
+    actionsPopUp.classList.toggle('opened')
+})
+}
